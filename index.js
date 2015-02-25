46edcd190c1536a3ccc9e21c11796b56f944f316
hex7c0 <carniellifrancesco@gmail.com>
2015-02-25 22:59:18 +0100

    main: fix absolute heade

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function mkdir(root, mode, callback) {
   var chunks = root.split(path.sep); // split in chunks
   var chunk;
   if (path.isAbsolute(root) === true) {
-    chunk = '/'; // build from absolute path
+    chunk = path.sep; // build from absolute path
   } else {
     chunk = path.resolve(); // build with relative path
   }
@@ -69,7 +69,7 @@ function mkdirSync(root, mode) {
   var chunks = root.split(path.sep); // split in chunks
   var chunk;
   if (path.isAbsolute(root) === true) {
-    chunk = '/'; // build from absolute path
+    chunk = path.sep; // build from absolute path
   } else {
     chunk = path.resolve(); // build with relative path
   }
